dadba919459dac88abbca7acb61e3941e1eae24f
Aaron Hamilton (Air) <aaronleon28@gmail.com>
2024-11-27 12:28:05 +0000

    CoffeeDetailsScreen.kt now scrolls when content gets too big for the screen and made sure that the button at the bottom cannot had any text

--- app/src/main/java/org/uniqstudio/uniqcoffee/ui/screens/CoffeeDetailsScreen.kt
+++ app/src/main/java/org/uniqstudio/uniqcoffee/ui/screens/CoffeeDetailsScreen.kt
@@ -8,11 +8,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerEventType.Companion.Scroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,34 +36,37 @@ fun CoffeeDetailsScreen(
 ) {
     Box(
         modifier = Modifier.fillMaxSize()
-            .padding(40.dp),
+            .padding(40.dp)
+            .verticalScroll(rememberScrollState()),
         contentAlignment = Alignment.Center
     ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            HeaderText(title, true, 60)
-            Text(
-                "$$price",
-                fontSize = 20.sp
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                HeaderText(title, true, 60)
+                Text(
+                    "$$price",
+                    fontSize = 20.sp
                 )
 
-            Spacer(modifier = Modifier.padding(20.dp))
+                Spacer(modifier = Modifier.padding(20.dp))
 
-            Image(painter = painterResource(image), contentDescription = stringResource(title))
-            DescriptionText(description)
-            HiddenDetailsBox(
-                R.string.milk_type,
-                milkType,
-                false,
-                R.string.milk_semi
-            )
-            HiddenDetailsBox(
-                R.string.kcal,
-                kcal,
-                true,
-                R.string.kcal_info
-            )
+                Image(painter = painterResource(image), contentDescription = stringResource(title))
+                DescriptionText(description)
+                HiddenDetailsBox(
+                    R.string.milk_type,
+                    milkType,
+                    false,
+                    R.string.milk_semi
+                )
+                HiddenDetailsBox(
+                    R.string.kcal,
+                    kcal,
+                    true,
+                    R.string.kcal_info
+                )
+                Spacer(modifier = Modifier.padding(20.dp))
+
+            }
         }
-    }
     BottomBarButton(R.string.add_to_cart, onClick)
 }
 
